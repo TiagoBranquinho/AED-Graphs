8c649fe241bca58dd686127c3c64661aba454b93
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-22 16:25:49 +0000

    Added .gitignore file

--- cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tiagobranquinho/Desktop/universidade/2ANO/AED/GRAPHS-AED/lib/googletest-master
-# Build directory: /Users/tiagobranquinho/Desktop/universidade/2ANO/AED/GRAPHS-AED/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
