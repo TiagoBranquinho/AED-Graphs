89629cbdbfb2d78d05379ba70e2a35add8230244
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-23 16:06:51 +0000

    Updated Data class tests

--- tests/DataTest.cpp
+++ tests/DataTest.cpp
@@ -17,11 +17,22 @@ TEST(test1, readStopsTest) {
     cout << d.getStop(78) << endl;
     cout << d.nodes.at("1CDR2") << endl;
 
+    string stop = "1AL5";
+    auto it =d.nodes.find(stop);
+    if (it == d.nodes.end())
+        cout << "stop " << stop << " not found" << endl << endl;
+    else
+        cout << it->first << " -> " << it->second << endl << endl;
+
+    EXPECT_EQ(it->first, stop);
     EXPECT_EQ(d.getStop(78), "ACST2");
     EXPECT_EQ(d.getNode("1CDR2"), 6);
     EXPECT_EQ(d.getLine("205"), "205 - CAMPANHÃ-CASTELO DO QUEIJO");
 
     d.clear();
+    cout << endl;
+    cout << "data class has been cleared!"<< endl;
+    cout << endl;
     cout << "size: " << d.nodes.size() << endl;
 
     try{
@@ -31,4 +42,11 @@ TEST(test1, readStopsTest) {
         cout << exception.what() << endl;
         cout << "Value not found!" << endl;
     }
+
+    it = d.nodes.find(stop);
+    if (it == d.nodes.end())
+        cout << "stop " << stop << " not found" << endl;
+    else
+        cout << it->first << " -> " << it->second << endl;
+    EXPECT_EQ(it, d.nodes.end());
 }
