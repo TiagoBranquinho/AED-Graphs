154c31d865e5defc383a51b5c30595a1dc79fe28
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-26 23:38:31 +0000

    Removed cmake-build-debug-coverage

--- cmake-build-debug-coverage/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alexandre/Documents/FEUP/2ano/AED/AED-Graphs/lib/googletest-master
-# Build directory: /home/alexandre/Documents/FEUP/2ano/AED/AED-Graphs/cmake-build-debug-coverage/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug-coverage/lib/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alexandre/Documents/FEUP/2ano/AED/AED-Graphs/lib/googletest-master/googletest
-# Build directory: /home/alexandre/Documents/FEUP/2ano/AED/AED-Graphs/cmake-build-debug-coverage/lib/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
